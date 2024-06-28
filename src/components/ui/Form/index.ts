c7762913165ee96f57577485564a9d98171974b0
Wellington Costa <wellingtonvell@gmail.com>
2024-06-28 20:52:00 -0300

    refactor(Form): decompose component into modular subcomponents for improved reusability and maintainability

---
+++ src/components/ui/Form/index.ts
@@ -0,0 +1,7 @@
+export { default as Form } from './Form';
+export { default as FormField, FormFieldContext } from './FormField';
+export { default as FormItem, FormItemContext } from './FormItem';
+export { default as FormLabel } from './FormLabel';
+export { default as FormControl } from './FormControl';
+export { default as FormMessage } from './FormMessage';
+export { default as FormDescription } from './FormDescription';
