8a8827613ca29de1007438103de13358e6903bde
Wellington Costa <wellingtonvell@gmail.com>
2024-06-29 00:43:45 -0300

    refactor(Input): decompose component into modular subcomponents for improved reusability and maintainability

--- src/components/ui/Input/Input.tsx
+++ src/components/ui/Input/Input.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import { cn } from '@/lib/utils';
 
 export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
@@ -20,4 +20,4 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
 );
 Input.displayName = 'Input';
 
-export { Input };
+export default Input;
