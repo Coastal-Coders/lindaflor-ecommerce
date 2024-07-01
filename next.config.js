926eefdc8614c9408c7b565e97961f2df7b7868e
L <100241773+jacksxx@users.noreply.github.com>
2024-07-01 14:36:41 -0300

    Fix

--- next.config.js
+++ next.config.js
@@ -1,20 +1,9 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
     formats: ['image/avif', 'image/webp'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.pexels.com',
-        port: '',
-      },
-    ],
-  },
-  compiler: {
-    removeConsole: process.env.NODE_ENV === 'production' ? {
-      exclude: ['error'],
-    } : false, // Desativa a remoção de console em desenvolvimento
   },
 };
 
