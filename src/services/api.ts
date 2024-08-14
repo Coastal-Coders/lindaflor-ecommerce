9957fc4f1cce229c002a7727d8be15648bdf2755
Breno Souza Costa <brenosouza.dev@gmail.com>
2024-08-14 17:00:10 -0300

    fix: endpoint withcredentials in api service

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,21 +3,22 @@ import { NODE_ENV, uri } from '@/constants/environment-variables';
 
 const apiInstance = axios.create({
   baseURL: uri[NODE_ENV],
+  withCredentials: true,
 });
 
 const api = (axios: AxiosInstance) => {
   return {
     get: function <T>(url: string, config: AxiosRequestConfig = {}) {
-      return axios.get<T>(url, config);
+      return axios.get<T>(url, { ...config, withCredentials: true });
     },
     put: function <T>(url: string, body: unknown, config: AxiosRequestConfig = {}) {
-      return axios.put<T>(url, body, config);
+      return axios.put<T>(url, body, { ...config, withCredentials: true });
     },
     post: function <T>(url: string, body: unknown, config: AxiosRequestConfig = {}) {
-      return axios.post<T>(url, body, config);
+      return axios.post<T>(url, body, { ...config, withCredentials: true });
     },
     delete: function <T>(url: string, config: AxiosRequestConfig = {}) {
-      return axios.delete<T>(url, config);
+      return axios.delete<T>(url, { ...config, withCredentials: true });
     },
   };
 };
